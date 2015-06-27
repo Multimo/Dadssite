ee733f3326d25306a65dd6ea61f164aed756ad5a
Tim Mulqueen <tim.mulqueen@gmail.com>
2015-06-26 20:45:36 -0400

    updating site to use on gh-pages

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+//modal
+
+$(document).ready(function(){
+	$(".contactbtn").on("click", function(){
+		$(".overlay").toggleClass("show");
+
+	})
+	$(".closebtn").on("click", function(){
+		$(".overlay").toggleClass("show");
+
+	})
+	
+});
+//paralax on about elements
+
+//email sender / verifier
